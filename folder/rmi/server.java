3ac59a239a627300f2187ffd96625836485e9d93
loc577 <113154677+loc577@users.noreply.github.com>
2025-06-09 09:03:05 +0700

    Update server.java

--- folder/rmi/server.java
+++ folder/rmi/server.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rmi;
 import java.rmi.Naming;
-/**
- *
- * @author ASUSTUFGAMING
- */
+
 public class server {
     public static void main(String[] args) throws Exception{
         tongtichimplement tt = new tongtichimplement();

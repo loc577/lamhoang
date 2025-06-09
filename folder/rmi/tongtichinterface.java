6f27c1dcfdcb89ec46a6c3517e4029fca68a74a2
loc577 <113154677+loc577@users.noreply.github.com>
2025-06-10 02:57:28 +0700

    Delete folder/rmi/tongtichinterface.java

--- folder/rmi/tongtichinterface.java
+++
@@ -1,9 +0,0 @@
-package rmi;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface tongtichinterface extends Remote{
-    public int tong(int n) throws RemoteException;
-    public int tich(int n) throws RemoteException;
-    
-}
